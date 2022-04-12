a30c4cdf9caf4390638f156979fdd162a315cac7
michael <kiselev.0353@gmail.com>
2022-04-12 12:45:23 +0300

    le commit

--- Task6/Class_description.h
+++ Task6/Class_description.h
@@ -1,22 +1,38 @@
+#pragma once
+#include <iostream>
+
 class Base {
 protected:
-  	virtual void SetHP(int hp);
-  	virtual void SetAtk(int atk);
-  	virtual void SetDef(int def);
-  	virtual void SetLevel(int lvl);
+    virtual void SetHP(int hp);
+
+    virtual void SetAtk(int atk);
+
+    virtual void SetDef(int def);
+
+    virtual void SetLevel(int lvl);
 
 public:
-  	const std::string name;
-  	Base(std::string Name);
-  ~Base();
-  virtual int GetHP() const;
-  virtual int GetAtk() const;
-  virtual int GetDef() const;
-  virtual int GetLevel() const;
-  virtual bool IsDead() const;
-  virtual void Victory();
-  void Attack(Base &other);
-  void Load(int hp, int atk, int def, int lvl);
+    const std::string name;
+
+    Base(std::string Name);
+
+    ~Base();
+
+    virtual int GetHP() const;
+
+    virtual int GetAtk() const;
+
+    virtual int GetDef() const;
+
+    virtual int GetLevel() const;
+
+    virtual bool IsDead() const;
+
+    virtual void Victory();
+
+    void Attack(Base &other);
+
+    void Load(int hp, int atk, int def, int lvl);
 };
 
 ////////////////////
@@ -24,28 +40,58 @@ public:
 ///////////////////
 
 class Character : public Base {
-  	int _hp;
-  	int _atk;
-  	int _def;
- 	int _lvl;
+    int _hp;
+    int _atk;
+    int _def;
+    int _lvl;
 
 public:
-  Character(std::string Name);
-  ~Character();
+    Character(std::string Name);
+
+    ~Character();
+
+    void SetHP(int hp) final;
+
+    void SetAtk(int atk) final;
+
+    void SetDef(int def) final;
+
+    void SetLevel(int lvl) final;
+
+    int GetHP() const final;
+
+    int GetAtk() const final;
+
+    int GetDef() const final;
+
+    int GetLevel() const final;
+
+    bool IsDead() const final;
 };
 
 class Player : public Character {
-int _xp;
+    int _xp;
 
+    int _potentialXpReward = 0;
 public:
- 	Player(std::string Name = "Player");
-  	~Player();
+    Player(std::string Name = "Player");
+
+    void SetXP(int xp);
+
+    int GetXP() const;
+
+    void Attack(Base &other);
+
+    void Victory() final;
+
+    ~Player();
 };
 
 class Enemy : public Character {
- 	int _xpReward;
+    int _xpReward;
 
 public:
-Enemy(std::string Name = "Enemy");
-  	~Enemy();
+    Enemy(std::string Name = "Enemy");
+
+    ~Enemy();
 };
--- Task6/Class_method.cpp
+++ Task6/Class_method.cpp
@@ -1,45 +1,123 @@
 #include "Class_description.h"
+#include <iostream>
+#include <utility>
 
 void Base::SetHP(int hp) {}
+
 void Base::SetAtk(int atk) {}
+
 void Base::SetDef(int def) {}
+
 void Base::SetLevel(int lvl) {}
+
 Base::Base(std::string Name)
-    : name(Name) {}
+        : name(std::move(Name)) {}
+
 Base::~Base() {}
+
 int Base::GetHP() const { return 0; }
+
 int Base::GetAtk() const { return 0; }
+
 int Base::GetDef() const { return 0; }
+
 int Base::GetLevel() const { return 0; }
+
 bool Base::IsDead() const { return true; }
+
 void Base::Victory() {}
-void Base::Attack(Base &other)
-{
-  if (IsDead())
-    return;
-  int dmg = std::max(1, GetAtk() - other.GetDef());
-  other.SetHP(other.GetHP() - dmg);
-  std::cout << name << " attacked " << other.name << " and dealt " << dmg << " damage!" << std::endl;
-  if (other.IsDead())
-  {
-    std::cout << name << " won!" << std::endl;
-    Victory();
-  }
+
+void Base::Attack(Base &other) {
+    if (IsDead())
+        return;
+    int dmg = std::max(1, GetAtk() - other.GetDef());
+    other.SetHP(other.GetHP() - dmg);
+    std::cout << name << " attacked " << other.name << " and dealt " << dmg << " damage!" << std::endl;
+    if (other.IsDead()) {
+        std::cout << name << " won!" << std::endl;
+        Victory();
+    }
 }
-void Base::Load(int hp, int atk, int def, int lvl)
-{
-  SetHP(hp);
-  SetAtk(atk);
-  SetDef(def);
-  SetLevel(lvl);
-  std::cout << "hp: " << GetHP() << std::endl;
-  std::cout << "atk: " << GetAtk() << std::endl;
-  std::cout << "def: " << GetDef() << std::endl;
-  std::cout << "lvl: " << GetLevel() << std::endl;
+
+void Base::Load(int hp, int atk, int def, int lvl) {
+    SetHP(hp);
+    SetAtk(atk);
+    SetDef(def);
+    SetLevel(lvl);
+    std::cout << "hp: " << GetHP() << std::endl;
+    std::cout << "atk: " << GetAtk() << std::endl;
+    std::cout << "def: " << GetDef() << std::endl;
+    std::cout << "lvl: " << GetLevel() << std::endl;
 }
+
 Player::Player(std::string Name) : Character(Name) {}
+
 Player::~Player() {}
+
+void Player::SetXP(int xp) {
+    Character::SetLevel(Character::GetLevel() + xp / 100);
+    _xp = xp % 100;
+}
+
+int Player::GetXP() const {
+    return _xp;
+}
+
+void Player::Attack(Base &other) {
+    _potentialXpReward = 5*other.GetLevel() + other.GetDef()/4 + other.GetAtk()/4;
+    Base::Attack(other);
+}
+
+void Player::Victory() {
+    Base::Victory();
+    SetXP(GetXP() + _potentialXpReward);
+    _potentialXpReward = 0;
+}
+
 Enemy::Enemy(std::string Name) : Character(Name) {}
+
 Enemy::~Enemy() {}
+
 Character::Character(std::string Name) : Base(Name) {}
+
 Character::~Character() {}
+
+void Character::SetHP(int hp) {
+    Base::SetHP(hp);
+    _hp = hp;
+}
+
+void Character::SetAtk(int atk) {
+    Base::SetAtk(atk);
+    _atk = atk;
+}
+
+void Character::SetDef(int def) {
+    Base::SetDef(def);
+    _def = def;
+}
+
+void Character::SetLevel(int lvl) {
+    Base::SetLevel(lvl);
+    _lvl = lvl;
+}
+
+int Character::GetHP() const {
+    return _hp;
+}
+
+int Character::GetAtk() const {
+    return _atk;
+}
+
+int Character::GetDef() const {
+    return _def;
+}
+
+int Character::GetLevel() const {
+    return _lvl;
+}
+
+bool Character::IsDead() const {
+    return GetHP() <= 0;
+}
